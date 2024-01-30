084e5844506312743d9e558972a9ce624efd8895
ZODIACDEVS <153129821+b4tudevs@users.noreply.github.com>
2024-01-30 06:10:22 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,31 +1,28 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 PlayerData = {}
 
+-- Kontrol edilecek silahlar
 Weapons = {
-    "WEAPON_GLOCK17",
+    "WEAPON_GLOCK",
     "WEAPON_STUNGUN",
     "WEAPON_PUMPSHOTGUN",
-    "WEAPON_MP5",
-    "WEAPON_HK416",
-    "WEAPON_SCARH",
+    "WEAPON_SMG",
+    "WEAPON_M4",
     "WEAPON_NIGHTSTICK",
     "WEAPON_FLASHLIGHT",
 }
 
+-- Meslekler
 Jobs = {
     "police",
 }
 
-CreateThread(function()
-    local QBCore = exports['qb-core']:GetCoreObject()
-    PlayerData = QBCore.Functions.GetPlayerData()
-    
-    isPlayerWhitelisted = refreshPlayerWhitelisted()
-end)
+local isPlayerWhitelisted = false
+local cooldownActive = false
 
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
-AddEventHandler('QBCore:Client:OnPlayerLoaded', function(xPlayer)
-	PlayerData = xPlayer
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    PlayerData = QBCore.Functions.GetPlayerData()
 end)
 
 RegisterNetEvent('QBCore:Client:OnJobUpdate')
@@ -34,39 +31,38 @@ AddEventHandler('QBCore:Client:OnJobUpdate', function(job)
     isPlayerWhitelisted = refreshPlayerWhitelisted()
 end)
 
-
-CreateThread(function()
+Citizen.CreateThread(function()
     while true do
-        Wait(500)
+        Wait(1000)
         local player = PlayerPedId()
-        if not isPlayerWhitelisted then
-            for k,v in pairs(Weapons) do
-                local player = PlayerPedId()
+
+        -- Eğer oyuncu whitelistlenmemişse ve cooldown süresi bitmişse
+        if not isPlayerWhitelisted and not cooldownActive then
+            for k, v in pairs(Weapons) do
                 local weapon = GetHashKey(v)
                 if HasPedGotWeapon(player, weapon, false) == 1 then
                     RemoveWeaponFromPed(player, weapon)
-                    QBCore.Functions.Notify("Bu silahı kullanamazsın.", "error")
+                    QBCore.Functions.Notify("Bu Silahı Kullanmaya Yetkin Yok Laa Gardaşşşş :).", "error")
                 end
             end
+            cooldownActive = true
+            Citizen.Wait(600000) -- 10 dakika cooldown süresi
+            cooldownActive = false
         end
     end
 end)
 
-
 function refreshPlayerWhitelisted()
-	if not PlayerData then
-		return false
-	end
+    if not PlayerData or not PlayerData.job then
+        return false
+    end
 
-	if not PlayerData.job then
-		return false
-	end
+    -- Eğer oyuncunun mesleği whitelistteki mesleklerden biriyse true döndür
+    for k, v in ipairs(Jobs) do
+        if v == PlayerData.job.name then
+            return true
+        end
+    end
 
-	for k,v in ipairs(Jobs) do
-		if v == PlayerData.job.name then
-			return true
-		end
-	end
-
-	return false
+    return false
 end
